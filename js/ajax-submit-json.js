64c90bf8854f05e68cfc37b0e4b52d91472129eb
Anderson <andersonrozwot@gmail.com>
2024-05-16 14:48:22 -0300

    nfinal

---
+++ js/ajax-submit-json.js
@@ -0,0 +1,28 @@
+// versao 2
+const resp = document.getElementById("resp")
+
+document.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const form = document.querySelector("form");
+    let cliente = new FormData();
+    cliente.append('codigo', form, inCodigo, value);
+    cliente.append('nome', form, inNome, value);
+    cliente.append('email', form, inEmail, value);
+
+    console.log(cliente);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", "cliente-inset-formdata.php")
+    req.onload = function () {
+        if (xhr.status == 200) {
+            //let resp = JSON.parse(this.responseText);
+            resp.innerText = xhr.responseText;
+            form.reset();
+        }
+        else {
+            resp.innerText = `Erro: ${req.status} ${req.statusText}`;
+        }
+    }
+    xhr.send(cliente);
+    e.preventDefault;
+})
